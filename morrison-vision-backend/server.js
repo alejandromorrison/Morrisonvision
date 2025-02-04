1766781dce6bba22b8f97c7bd3de2cc1d061907c
Alejandro Morrison <nelsonmorrisonhidalgo0@gmail.com>
2025-02-04 14:25:25 -0400

    adding the backend to specify later the new tasks to add the AWS to the account!

---
+++ morrison-vision-backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my node .js app!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
